19fc35021549eb5eef4f0dc8669c0dc7bc183c50
Manjunatha J <manjunathaj259@gmail.com>
2026-04-01 21:30:43 +0530

    Reorganize imports and maintain app structure

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -26,7 +27,8 @@ def home():
 
     return render_template('home.html', weather=weather)
 
-if __name__ == '__main__':
-    import os
+
 port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
